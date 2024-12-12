801abf887454854b77e0d0a7d9cd684aa4b41809
Andrew Gari <covadax.ag@gmail.com>
2024-12-12 18:20:33 -0500

    rest

---
+++ cargo-tutorial/target/debug/cargo-tutorial.d
@@ -0,0 +1,1 @@
+/home/covadax/workspace/rust/cargo-tutorial/target/debug/cargo-tutorial: /home/covadax/workspace/rust/cargo-tutorial/src/main.rs
---
+++ cargo-tutorial/target/debug/deps/cargo_tutorial-4fad894878dde4f4.d
@@ -0,0 +1,5 @@
+/home/covadax/workspace/rust/cargo-tutorial/target/debug/deps/libcargo_tutorial-4fad894878dde4f4.rmeta: src/main.rs
+
+/home/covadax/workspace/rust/cargo-tutorial/target/debug/deps/cargo_tutorial-4fad894878dde4f4.d: src/main.rs
+
+src/main.rs:
