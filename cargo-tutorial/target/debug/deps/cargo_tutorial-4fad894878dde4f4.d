84a521336555885c3f7d4b5693dca1712e8376b7
Andrew Gari <covadax.ag@gmail.com>
2024-12-19 18:27:25 -0500

    Removed unrelated code

--- cargo-tutorial/target/debug/deps/cargo_tutorial-4fad894878dde4f4.d
+++
@@ -1,5 +0,0 @@
-/home/covadax/Repos/the-rust-programming-language/cargo-tutorial/target/debug/deps/libcargo_tutorial-4fad894878dde4f4.rmeta: src/main.rs
-
-/home/covadax/Repos/the-rust-programming-language/cargo-tutorial/target/debug/deps/cargo_tutorial-4fad894878dde4f4.d: src/main.rs
-
-src/main.rs:
--- cargo-tutorial/target/debug/deps/cargo_tutorial-fcebefc552116d33.d
+++
@@ -1,5 +0,0 @@
-/home/covadax/Repos/the-rust-programming-language/cargo-tutorial/target/debug/deps/libcargo_tutorial-fcebefc552116d33.rmeta: src/main.rs
-
-/home/covadax/Repos/the-rust-programming-language/cargo-tutorial/target/debug/deps/cargo_tutorial-fcebefc552116d33.d: src/main.rs
-
-src/main.rs:
